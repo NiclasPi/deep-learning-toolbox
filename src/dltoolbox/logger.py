02e0dc45f6cf4f5470bd02c0eb3ca5e1c6acbe54
Niclas Pillath <npillath@stimlabs.dev>
2024-12-10 19:45:22 +0100

    Add Logger utility class

---
+++ src/dltoolbox/logger.py
@@ -0,0 +1,13 @@
+import os
+from typing import Optional
+
+
+class Logger:
+    def __init__(self, log_file: Optional[str] = None):
+        self._log_file_path = os.path.abspath(log_file) if log_file is not None else ""
+        self._debug = __debug__ or log_file is None
+
+    def log(self, *values: object) -> None:
+        print(*values,
+              file=open(self._log_file_path, "a") if not self._debug else None,
+              flush=True)
